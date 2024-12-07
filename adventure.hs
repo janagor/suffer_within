0fde8115046635fda8f712a802013f9383f8bbcd
Jan Górski <jan.a.gorski@wp.pl>
2024-12-07 16:45:20 +0100

    interactables connected with rooms

--- adventure.hs
+++ adventure.hs
@@ -1,5 +1,5 @@
--- The germ of a text adventure game
--- Marcin Szlenk 2024
+-- Suffer within - game by Jakub Antas, Jan Górski, Patryk Zdziech
+
 
 introductionText = [
     "A long time ago, in a galaxy far, far away...",
@@ -49,16 +49,24 @@ readCommand = do
 --------------------------------------------------------------------------------
 
 -- Definicja typu Location
-data Location = Location { name :: String, north :: Maybe Location, south :: Maybe Location, east :: Maybe Location, west :: Maybe Location }
+data Location = Location { name :: String, north :: Maybe LocationWItems, south :: Maybe LocationWItems, east :: Maybe LocationWItems, west :: Maybe LocationWItems }
+type Interactables = [String]
+data LocationWItems = LocationWItems { location :: Location, interactables :: Interactables }
+
+displayInteractables :: Interactables -> IO ()
+displayInteractables xs = putStr (unlines ("Interactables:" : xs))
+
 
 -- Funkcja do zmiany pokoju na podstawie kierunku
-moveT :: Location -> Char -> Location
-moveT currentLocation direction = case direction of
-    'n' -> maybe currentLocation id (north currentLocation)
-    's' -> maybe currentLocation id (south currentLocation)
-    'e'  -> maybe currentLocation id (east currentLocation)
-    'w'  -> maybe currentLocation id (west currentLocation)
-    _       -> currentLocation
+moveT :: LocationWItems -> Char -> LocationWItems
+moveT currentLocation direction = do
+    let loc = location currentLocation
+    case direction of
+        'n' -> maybe currentLocation id (north loc)
+        's' -> maybe currentLocation id (south loc)
+        'e'  -> maybe currentLocation id (east loc)
+        'w'  -> maybe currentLocation id (west loc)
+        _       -> currentLocation
 
 -- Funkcja do wyświetlania pokoju
 displayLocation :: Location -> IO ()
@@ -66,27 +74,44 @@ displayLocation location = putStrLn $ "You are in the " ++ name location
 
 -- LOKALIZACJE
 garden :: Location
-garden = Location { name = "Garden", north = Just hall, south = Nothing, east = Just glasshouse, west = Nothing }
-hall ::Location 
-hall = Location { name = "Hall", north = Nothing, south = Just garden, east = Just banquetHall, west = Just library }
-glasshouse ::Location 
-glasshouse = Location { name = "Glasshouse", north = Nothing, south = Nothing, east = Nothing, west = Just garden }
-library ::Location 
-library = Location { name = "Library", north = Nothing, south = Nothing, east = Just hall, west = Nothing }
-banquetHall ::Location 
-banquetHall = Location { name = "Banquet Hall", north = Nothing, south = Nothing, east = Just closet, west = Just hall }
-closet ::Location 
-closet = Location { name = "Closet", north = Nothing, south = Nothing, east = Nothing, west = Just banquetHall }
+garden = Location { name = "Garden", north = Just hallW, south = Nothing, east = Just glasshouseW, west = Nothing }
+hall :: Location
+hall = Location { name = "Hall", north = Nothing, south = Just gardenW, east = Just banquetHallW, west = Just libraryW }
+glasshouse :: Location
+glasshouse = Location { name = "Glasshouse", north = Nothing, south = Nothing, east = Nothing, west = Just gardenW }
+library :: Location
+library = Location { name = "Library", north = Nothing, south = Nothing, east = Just hallW, west = Nothing }
+banquetHall :: Location
+banquetHall = Location { name = "Banquet Hall", north = Nothing, south = Nothing, east = Just closetW, west = Just hallW }
+closet :: Location
+closet = Location { name = "Closet", north = Nothing, south = Nothing, east = Nothing, west = Just banquetHallW }
 
+-- LOKALIZACJE Z ITEMAMY
+gardenW :: LocationWItems
+gardenW = LocationWItems { location = garden, interactables = [] }
+hallW :: LocationWItems
+hallW = LocationWItems { location = hall, interactables = [] }
+glasshouseW :: LocationWItems
+glasshouseW = LocationWItems { location = glasshouse, interactables = ["slingshot"] }
+libraryW :: LocationWItems
+libraryW = LocationWItems { location = library, interactables = ["piece_of_paper", "window", "bool"] }
+banquetHallW :: LocationWItems
+banquetHallW = LocationWItems { location = banquetHall, interactables = [ "painting", "chandelier", "skeletons" ] }
+closetW :: LocationWItems
+closetW = LocationWItems { location = closet, interactables = ["crystal ball", "floor"] }
 
-moveRecursively :: Location -> IO ()
+moveRecursively :: LocationWItems -> IO ()
 moveRecursively pos = do
-    displayLocation pos
+    let position = location pos
+    let int = interactables pos
+    displayLocation position
+    displayInteractables int
     putStr "Enter direction (n/s/e/w, or 'q' to quit): "
     command <- getLine
     case command of
         "q" -> putStrLn "Goodbye!"
-        _ -> do moveRecursively (foldl moveT pos command)
+        _ -> do
+            moveRecursively (foldl moveT pos command)
 
 gameLoop :: IO ()
 gameLoop = do
@@ -101,5 +126,5 @@ gameLoop = do
 main = do
     printIntroduction
     printInstructions
-    moveRecursively garden
+    moveRecursively gardenW
 
