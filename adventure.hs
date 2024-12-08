999951166c22626cf3e28735403a14d5b22516d8
Jan Górski <jan.a.gorski@wp.pl>
2024-12-08 19:39:33 +0100

    cleared and added fag field example

--- adventure.hs
+++ adventure.hs
@@ -1,130 +1,167 @@
 -- Suffer within - game by Jakub Antas, Jan Górski, Patryk Zdziech
 
+import Data.Maybe
 
-introductionText = [
-    "A long time ago, in a galaxy far, far away...",
-    "",
-    "It is a period of civil war. Rebel",
-    "spaceships, striking from a hidden",
-    "base, have won their first victory",
-    "against the evil Galactic Empire.",
-    "",
-    "During the battle, Rebel spies managed",
-    "to steal secret plans to the Empire's",
-    "ultimate weapon, the Death Star, an",
-    "armored space station with enough",
-    "power to destroy an entire planet.",
-    "",
-    "Pursued by the Empire's sinister agents,",
-    "Princess Leia races home aboard her",
-    "starship, custodian of the stolen plans",
-    "that can save her people and restore",
-    "freedom to the galaxy....",
-    ""
-    ]
+--------------------------------------------------------------------------------
+-- TYPES -----------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
-instructionsText = [
-    "Available commands are:",
-    "",
-    "instructions  -- to see these instructions.",
-    "quit          -- to end the game and quit.",
-    ""
-    ]
+type Interactables = [String]
+
+data Location = Location
+  { name :: String,
+    interactables :: Interactables,
+    north :: Maybe Location,
+    south :: Maybe Location,
+    east :: Maybe Location,
+    west :: Maybe Location
+  }
+
+newtype Flags = Flags
+  { -- isBirdKilled :: Bool,
+    isHallOpen :: Bool
+    -- isEngineOn :: Bool
+  }
+
+data State = State {location :: Location, flags :: Flags}
+
+--------------------------------------------------------------------------------
+-- CONSTANTS -------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- stany -----------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+start = State {location = garden, flags = Flags {isHallOpen = False}}
+
+--------------------------------------------------------------------------------
+-- lokalizacje -----------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+garden :: Location
+garden =
+  Location
+    { name = "Garden",
+      interactables = [],
+      north = Just hall,
+      south = Nothing,
+      east = Just glasshouse,
+      west = Nothing
+    }
+
+hall :: Location
+hall =
+  Location
+    { name = "Hall",
+      interactables = [],
+      north = Nothing,
+      south = Just garden,
+      east = Just banquetHall,
+      west = Just library
+    }
+
+glasshouse :: Location
+glasshouse =
+  Location
+    { name = "Glasshouse",
+      interactables = ["slingshot"],
+      north = Nothing,
+      south = Nothing,
+      east = Nothing,
+      west = Just garden
+    }
+
+library :: Location
+library =
+  Location
+    { name = "Library",
+      interactables = ["piece_of_paper", "window", "bool"],
+      north = Nothing,
+      south = Nothing,
+      east = Just hall,
+      west = Nothing
+    }
+
+banquetHall :: Location
+banquetHall =
+  Location
+    { name = "Banquet Hall",
+      interactables = ["painting", "chandelier", "skeletons"],
+      north = Nothing,
+      south = Nothing,
+      east = Just closet,
+      west = Just hall
+    }
+
+closet :: Location
+closet =
+  Location
+    { name = "Closet",
+      interactables = ["crystal ball", "floor"],
+      north = Nothing,
+      south = Nothing,
+      east = Nothing,
+      west = Just banquetHall
+    }
+
+--------------------------------------------------------------------------------
+-- FUNKCJE ---------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 -- print strings from list in separate lines
 printLines :: [String] -> IO ()
 printLines xs = putStr (unlines xs)
-                  
-printIntroduction = printLines introductionText
-printInstructions = printLines instructionsText
 
 readCommand :: IO String
 readCommand = do
-    putStr "> "
-    xs <- getLine
-    return xs
-
---------------------------------------------------------------------------------
--- Locations -------------------------------------------------------------------
---------------------------------------------------------------------------------
-
--- Definicja typu Location
-data Location = Location { name :: String, north :: Maybe LocationWItems, south :: Maybe LocationWItems, east :: Maybe LocationWItems, west :: Maybe LocationWItems }
-type Interactables = [String]
-data LocationWItems = LocationWItems { location :: Location, interactables :: Interactables }
+  putStr "> "
+  xs <- getLine
+  return xs
 
 displayInteractables :: Interactables -> IO ()
 displayInteractables xs = putStr (unlines ("Interactables:" : xs))
 
-
 -- Funkcja do zmiany pokoju na podstawie kierunku
-moveT :: LocationWItems -> Char -> LocationWItems
-moveT currentLocation direction = do
-    let loc = location currentLocation
-    case direction of
-        'n' -> maybe currentLocation id (north loc)
-        's' -> maybe currentLocation id (south loc)
-        'e'  -> maybe currentLocation id (east loc)
-        'w'  -> maybe currentLocation id (west loc)
-        _       -> currentLocation
+move :: Location -> Char -> Location
+move currentLocation direction = do
+  case direction of
+    'n' -> fromMaybe currentLocation (north currentLocation)
+    's' -> fromMaybe currentLocation (south currentLocation)
+    'e' -> fromMaybe currentLocation (east currentLocation)
+    'w' -> fromMaybe currentLocation (west currentLocation)
+    _ -> currentLocation
 
 -- Funkcja do wyświetlania pokoju
 displayLocation :: Location -> IO ()
 displayLocation location = putStrLn $ "You are in the " ++ name location
 
--- LOKALIZACJE
-garden :: Location
-garden = Location { name = "Garden", north = Just hallW, south = Nothing, east = Just glasshouseW, west = Nothing }
-hall :: Location
-hall = Location { name = "Hall", north = Nothing, south = Just gardenW, east = Just banquetHallW, west = Just libraryW }
-glasshouse :: Location
-glasshouse = Location { name = "Glasshouse", north = Nothing, south = Nothing, east = Nothing, west = Just gardenW }
-library :: Location
-library = Location { name = "Library", north = Nothing, south = Nothing, east = Just hallW, west = Nothing }
-banquetHall :: Location
-banquetHall = Location { name = "Banquet Hall", north = Nothing, south = Nothing, east = Just closetW, west = Just hallW }
-closet :: Location
-closet = Location { name = "Closet", north = Nothing, south = Nothing, east = Nothing, west = Just banquetHallW }
+gameLoop :: State -> IO ()
+gameLoop state = do
+  let loc = location state
+  displayLocation loc
 
--- LOKALIZACJE Z ITEMAMY
-gardenW :: LocationWItems
-gardenW = LocationWItems { location = garden, interactables = [] }
-hallW :: LocationWItems
-hallW = LocationWItems { location = hall, interactables = [] }
-glasshouseW :: LocationWItems
-glasshouseW = LocationWItems { location = glasshouse, interactables = ["slingshot"] }
-libraryW :: LocationWItems
-libraryW = LocationWItems { location = library, interactables = ["piece_of_paper", "window", "bool"] }
-banquetHallW :: LocationWItems
-banquetHallW = LocationWItems { location = banquetHall, interactables = [ "painting", "chandelier", "skeletons" ] }
-closetW :: LocationWItems
-closetW = LocationWItems { location = closet, interactables = ["crystal ball", "floor"] }
+  let inter = interactables loc
+  displayInteractables inter
 
-moveRecursively :: LocationWItems -> IO ()
-moveRecursively pos = do
-    let position = location pos
-    let int = interactables pos
-    displayLocation position
-    displayInteractables int
-    putStr "Enter direction (n/s/e/w, or 'q' to quit): "
-    command <- getLine
-    case command of
-        "q" -> putStrLn "Goodbye!"
+  putStr "Enter direction (n/s/e/w, or 'q' to quit): "
+  command <- getLine
+  case command of
+    "q" -> putStrLn "Goodbye!"
+    _ -> do
+      let local = foldl move loc command
+      let flgs = flags state
+
+      let isopen = isHallOpen flgs
+
+      case local of
+        Location "Library" _ _ _ _ _ -> do
+          putStrLn "You are in the Library!"
+          let whatever = State {location = local, flags = Flags {isHallOpen = False}}
+          gameLoop whatever
         _ -> do
-            moveRecursively (foldl moveT pos command)
-
-gameLoop :: IO ()
-gameLoop = do
-    cmd <- readCommand
-    case cmd of
-        "instructions" -> do printInstructions
-                             gameLoop
-        "quit" -> return ()
-        _ -> do printLines ["Unknown command.", ""]
-                gameLoop
+          let whatever = State {location = local, flags = flags state}
+          gameLoop whatever
 
 main = do
-    printIntroduction
-    printInstructions
-    moveRecursively gardenW
-
+  gameLoop start
